8291ebc90b843b4a1b334f9004a75d95ef529cc0
guicbelon <guilherme.c.belon@gmail.com>
2024-05-15 10:12:13 -0300

    add: pressure plots and velocity plots

--- meccomp_lib/ex2/mesh.py
+++ meccomp_lib/ex2/mesh.py
@@ -4,12 +4,16 @@ import seaborn as sns
 from .point import Point
 
 class Mesh:
-    def __init__(self, delta:float = 0.01, rotation_angle:float=0, U_infinity:float=30):
+    def __init__(self, delta:float = 0.01, 
+                    rotation_angle:float=0, 
+                    U_infinity:float=30, 
+                    max_num_of_iterations:float=10E5):
         self.delta = delta
         self.lambda_value = 1.85
         self.tolerance = 0.01
         self.rotation_angle = rotation_angle*np.pi/180
         self.U_infinity = U_infinity
+        self.max_num_of_iterations = max_num_of_iterations
         self._preprocess()
     
     def _preprocess(self):
@@ -33,7 +37,7 @@ class Mesh:
             for r in range(self.num_of_rows):
                 new_x = self.delta * c
                 new_y = self.delta * r
-                new_point = Point(x_pos = new_x, y_pos = new_y)
+                new_point = Point(x_pos = new_x, y_pos = new_y, U_infinity=self.U_infinity)
                 is_airfoil = self.is_in_airfoil(new_point)
                 new_point.is_airfoil = is_airfoil
                 if(r == 0 or c == 0) or (r == (self.num_of_rows-1) or c == (self.num_of_cols-1)):
@@ -51,40 +55,22 @@ class Mesh:
         if in_x and in_y:
             return True
         return False
-    
-    def plot_mesh(self): 
-        mesh = [[None]*self.num_of_cols for _ in range(self.num_of_rows)]
-        for c in range(self.num_of_cols):
-            for r in range(self.num_of_rows):
-                point = self.mesh[r][c]
-                mesh[r][c] = not point.is_airfoil
-        plt.imshow(mesh, cmap='gray', interpolation='nearest')
-        plt.title('Posição do Aerofólio')
-        plt.axis('off')
-        plt.show()
-    
-    def plot_streamline(self):
-        mesh_values = self.get_mesh_values()  
-        mesh_values = np.array(mesh_values)
-        mask = np.zeros_like(mesh_values, dtype=bool)
-        mask[mesh_values == 0] = True
-        plt.figure(figsize=(10, 8))
-        sns.heatmap(mesh_values, annot=False, cmap='coolwarm', linewidths=0, mask=mask, cbar_kws={'extend': 'both'})
-        nonzero_values = mesh_values[mesh_values != 0]
-        levels = np.percentile(nonzero_values, np.linspace(0, 100, num=10))  
-        plt.contour(mesh_values, levels=levels, colors='k', linestyles='solid', linewidths=2, alpha=0.7,
-                    interpolation='bilinear', extent=(0, mesh_values.shape[1], 0, mesh_values.shape[0]))
-        plt.title('Linha de Corrente para o Escoamento')
-        plt.axis('off')
-        plt.show() 
         
-
-        
-    def get_mesh_values(self):  
+    def get_parameter_value(self, 
+                            streamline:bool=False, 
+                            velocity:bool=False, 
+                            pressure:bool=False):  
         mesh = [[None]*self.num_of_cols for _ in range(self.num_of_rows)]
         for row_id in range(self.num_of_rows): 
             for col_id in range(self.num_of_cols):   
-                mesh[row_id][col_id] = self.mesh[row_id][col_id].streamline
+                if streamline:
+                    mesh[row_id][col_id] = self.mesh[row_id][col_id].streamline
+                elif velocity:
+                    mesh[row_id][col_id] = self.mesh[row_id][col_id].absolute_velocity
+                elif pressure:
+                    mesh[row_id][col_id] = self.mesh[row_id][col_id].pressure
+                else:
+                    raise Exception("Must set a parameter to get value")
         return mesh
         
     def apply_overelaxing(self, old_value, calculated_value):
@@ -127,18 +113,20 @@ class Mesh:
                                     self.right_point.streamline +
                                     2*self.upper_point.streamline
                                     -2*self.delta*self.U_infinity)/4
-        elif self.left_point is None and ((self.upper_point is not None) and (self.lower_point is not None)):
+        elif self.left_point is None and ((self.upper_point is not None) 
+                                          and (self.lower_point is not None)):
             calculated_value = (2*self.right_point.streamline 
                                 +self.lower_point.streamline 
                                 + self.upper_point.streamline)/4
-        elif self.right_point is None and ((self.upper_point is not None) and (self.lower_point is not None)):
+        elif self.right_point is None and ((self.upper_point is not None) 
+                                           and (self.lower_point is not None)):
             calculated_value = (2*self.left_point.streamline 
                                 +self.lower_point.streamline 
                                 + self.upper_point.streamline)/4
         
         return calculated_value
     
-    def iter(self, row_id, col_id):
+    def iter_streamline(self, row_id, col_id):
         if row_id == 0:
             self.upper_point = None
         else: 
@@ -174,28 +162,112 @@ class Mesh:
         (self.mesh[row_id][col_id]).set_streamline(new_streamline_value)
         
         return error
-            
+    
+
+    def calculate_derivates(self, row_id, col_id):
+        center_point = self.mesh[row_id][col_id]
+        if center_point.is_border or center_point.is_airfoil:
+            self.mesh[row_id][col_id].calculate_absolute_velocity()
+            self.mesh[row_id][col_id].calculate_pressure()
+            return
+        del_in_x =(self.right_point.streamline - self.left_point.streamline)/(2*self.delta)
+        del_in_y = (self.upper_point.streamline - self.lower_point.streamline)/(2*self.delta)
+        self.mesh[row_id][col_id].set_del_streamline_del_x(del_in_x)
+        self.mesh[row_id][col_id].set_del_streamline_del_y(del_in_y)
+        self.mesh[row_id][col_id].calculate_absolute_velocity()
+        self.mesh[row_id][col_id].calculate_pressure()
+    
+    def calculate_velocity_and_pressure(self):
+        for row_id in range(self.num_of_rows): 
+            for col_id in range(self.num_of_cols):   
+                if row_id == 0:
+                    self.upper_point = None
+                else: 
+                    self.upper_point = self.mesh[row_id-1][col_id]
+                if row_id == (self.num_of_rows-1):
+                    self.lower_point = None
+                else:
+                    self.lower_point = self.mesh[row_id+1][col_id]
+                    
+                if col_id == 0:
+                    self.left_point = None
+                else:
+                    self.left_point = self.mesh[row_id][col_id-1]
+                if col_id == (self.num_of_cols-1):
+                    self.right_point = None
+                else:
+                    self.right_point = self.mesh[row_id][col_id+1]
+                self.calculate_derivates(row_id, col_id)
+                    
         
     def simul(self):
-        max_error = np.inf
-        num_of_cols = len(self.mesh[0])    
-        num_of_rows = len(self.mesh)   
-        while((max_error > self.tolerance) or (self.num_of_iterations>10000)): 
-            temp_error = 0
-                
-            for row_id in range(num_of_rows): 
-                for col_id in range(num_of_cols):   
+        max_error = np.inf 
+        while((max_error > self.tolerance) and (self.num_of_iterations<self.max_num_of_iterations)): 
+            temp_error = 0  
+            for row_id in range(self.num_of_rows): 
+                for col_id in range(self.num_of_cols):   
                     
                     #print('\n',row_id,col_id)
-                    error = self.iter(row_id,col_id)
+                    error = self.iter_streamline(row_id,col_id)
                     if error > temp_error:
                         temp_error = error
                     #print((self.mesh[row_id][col_id]).streamline, error)
             self.num_of_iterations+=1
             max_error = temp_error
-            print('\nmax error: ', max_error)
-            print('iteration: ', self.num_of_iterations)
+            print(f"Iteration: {self.num_of_iterations} - Max Error: {max_error}")
+        self.calculate_velocity_and_pressure()
         
-        
-        
+    def plot_mesh(self): 
+        mesh = [[None]*self.num_of_cols for _ in range(self.num_of_rows)]
+        for c in range(self.num_of_cols):
+            for r in range(self.num_of_rows):
+                point = self.mesh[r][c]
+                mesh[r][c] = not point.is_airfoil
+        plt.imshow(mesh, cmap='gray', interpolation='nearest')
+        plt.title('Posição do Aerofólio')
+        plt.axis('off')
+        plt.show()
     
+    def plot_streamline(self):
+        mesh_values = self.get_parameter_value(streamline=True)  
+        mesh_values = np.array(mesh_values)
+        mask = np.zeros_like(mesh_values, dtype=bool)
+        mask[mesh_values == 0] = True
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(mesh_values, annot=False, cmap='coolwarm', linewidths=0, mask=mask, cbar_kws={'extend': 'both'})
+        nonzero_values = mesh_values[mesh_values != 0]
+        levels = np.percentile(nonzero_values, np.linspace(0, 100, num=30))  
+        plt.contour(mesh_values, levels=levels, colors='k', linestyles='solid', linewidths=2, alpha=0.7,
+                    extent=(0, mesh_values.shape[1], 0, mesh_values.shape[0]))
+        plt.title('Linha de Corrente para o Escoamento')
+        plt.axis('off')
+        plt.show() 
+    
+    def plot_velocities(self):
+        mesh_values = self.get_parameter_value(velocity=True)
+        mesh_values = np.array(mesh_values)
+        mask = np.zeros_like(mesh_values, dtype=bool)
+        mask[mesh_values == 0] = True
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(mesh_values, annot=False, cmap='turbo', linewidths=0, mask=mask, cbar_kws={'extend': 'both'})
+        plt.title('Velocidade Absoluta')
+        plt.axis('off')
+        plt.show()
+        
+    def plot_pressure(self):
+        mesh_values = self.get_parameter_value(pressure=True)
+        mesh_values = np.array(mesh_values)
+        mask = np.zeros_like(mesh_values, dtype=bool)
+        mask[mesh_values == 0] = True
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(mesh_values, annot=False, cmap='Spectral', linewidths=0, mask=mask, cbar_kws={'extend': 'both'})
+        plt.title('Velocidade Absoluta')
+        plt.axis('off')
+        plt.show()
+    
+    
+
+        
+        
+        
+ 
--- meccomp_lib/ex2/point.py
+++ meccomp_lib/ex2/point.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Point:
     def __init__(self, 
                  x_pos:float=None, 
@@ -10,9 +12,51 @@ class Point:
         self.is_border = is_border
         self.is_airfoil = is_airfoil
         self.U_infinity = U_infinity
+        self._preprocess()
+    
+    def _preprocess(self):
         self.streamline = 0
+        self.del_streamline_del_x_value = 0
+        self.del_streamline_del_y_value = 0
+        self.pressure = 0
+        self.absolute_velocity = 0
+        self.g = 9.8
+        self.density = 1
+        self.bernoulli_result = 10E5/(self.density*self.g)
         
     def set_streamline(self, streamline):
         if self.is_airfoil:
             streamline = 0
         self.streamline = streamline
+        
+    def get_del_streamline_del_x(self):
+        if self.is_border or self.is_airfoil:
+            return 0
+        return self.del_streamline_del_x_value
+    
+    def set_del_streamline_del_x(self, stream_line):
+        if (not self.is_airfoil) and (not self.is_border):
+            self.del_streamline_del_x_value = stream_line
+    
+    def get_del_streamline_del_y(self):
+        if self.is_border:
+            return self.U_infinity
+        elif self.is_airfoil:
+            return 0
+        return self.del_streamline_del_y_value
+    
+    def set_del_streamline_del_y(self, stream_line):
+        if (not self.is_airfoil) and (not self.is_border):
+            self.del_streamline_del_y_value = stream_line
+            
+    def calculate_absolute_velocity(self):
+        del_in_x = self.get_del_streamline_del_x()
+        del_in_y = self.get_del_streamline_del_y()
+        self.absolute_velocity = np.sqrt(del_in_x**2 + del_in_y**2)
+
+    def calculate_pressure(self):
+        if self.is_airfoil:
+            return
+        self.pressure = self.density*self.g*(
+            self.bernoulli_result - (self.absolute_velocity**2)/2*self.g)
+        
